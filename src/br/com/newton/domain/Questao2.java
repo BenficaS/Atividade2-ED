1a33f47105e433676a05a67a08915ba075d7ac35
12111280 <benfica.infantaria@gmail.com>
2023-09-08 22:26:17 -0300

    cria: lista,txt.

---
+++ src/br/com/newton/domain/Questao2.java
@@ -0,0 +1,40 @@
+package br.com.newton.domain;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Questao2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        LinkedList<String> listaNoiva = new LinkedList<String>();
+        LinkedList<String> listanoivo = new LinkedList<String>();
+
+        Scanner scannerNoiva = new Scanner(new File("src/br/com/newton/domain/noiva.txt"));
+        while (scannerNoiva.hasNextLine()) {
+            listaNoiva.add(scannerNoiva.nextLine());
+        }
+
+        Scanner scannerNoivo = new Scanner(new File("src/br/com/newton/domain/noivo.txt"));
+        while (scannerNoivo.hasNextLine()){
+            listanoivo.add(scannerNoivo.nextLine());
+        }
+        LinkedList<String> convidados = new LinkedList<>(listaNoiva);
+        for (String nome: listanoivo) {
+            convidados.add(nome);
+        }
+        for (String listaConvidados: convidados) {
+            System.out.println(listaConvidados);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
