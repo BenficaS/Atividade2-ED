8f25ae12c03eda75380abc328efbe7ff23000ef7
12111280 <benfica.infantaria@gmail.com>
2023-09-08 22:39:24 -0300

    cria: lista, senhas, novo.

--- src/br/com/newton/domain/Questao2.java
+++ src/br/com/newton/domain/Questao2.java
@@ -2,9 +2,7 @@ package br.com.newton.domain;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Questao2 {
     public static void main(String[] args) throws FileNotFoundException {
@@ -17,18 +15,24 @@ public class Questao2 {
         }
 
         Scanner scannerNoivo = new Scanner(new File("src/br/com/newton/domain/noivo.txt"));
-        while (scannerNoivo.hasNextLine()){
+        while (scannerNoivo.hasNextLine()) {
             listanoivo.add(scannerNoivo.nextLine());
         }
         LinkedList<String> convidados = new LinkedList<>(listaNoiva);
-        for (String nome: listanoivo) {
-            convidados.add(nome);
-        }
-        for (String listaConvidados: convidados) {
-            System.out.println(listaConvidados);
+        //for (String nome: listanoivo) {
+        //convidados.add(nome);
+        convidados.addAll(listanoivo);
+        //adiciona lista de convidados
+
+        Collections.sort(convidados);
+        //ordem alfabetica
+        for (String nome : convidados) {
+            System.out.println(nome);
 
         }
+
     }
+
 }
 
 
